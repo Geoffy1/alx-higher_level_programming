72b883222ca6a70fe9eee606503058d0009c0f46
Geoffy1 <geojogu@gmail.com>
2022-09-22 18:48:10 -0700

    my calc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,18 +1,46 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    if len(argv) != 4:
-         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-         quit(1)
-         a = int(argv[1])
-         b = int(argv[3])
-         ops = ["+", "-", "*", "/"]
-         from calculator_1 import add, sub, mul, div
-         funcs = [add, sub, mul, div]
-         for i, s in enumerate(ops):
-             if argv[2] == s:
-                 print("{} {} {} = {}".format(a, s, b, funcs[i](a, b)))
-                 break
-             else:
-                 print("Unknown operator. Available operators: +, -, * and /")
-                 quit(1)
+    from calculator_1 import add, sub, mul, div
+    my_count = len(argv)
+
+    if my_count != 4:
+        print("Usage: {} <a> <operator> <b>".format(argv[0]))
+        quit(1)
+
+    a = int(argv[1])
+    b = int(argv[3])
+    ops = ["+", "-", "*", "/"]
+    
+    def not_found():
+        print("Unknown operator. Available operators: +, -, * and /")
+        quit(1)
+
+    def my_add():
+        total = add(num1, num2)
+        print("{:d} + {:d} = {:d}".format(num1, num2, total))
+        return total
+
+    def my_sub():
+        total = sub(num1, num2)
+        print("{:d} - {:d} = {:d}".format(num1, num2, total))
+        return total
+
+    def de_mul():
+        total = mul(num1, num2)
+        print("{:d} * {:d} = {:d}".format(num1, num2, total))
+        return total
+
+    def de_div():
+        total = div(num1, num2)
+        print("{:d} / {:d} = {:d}".format(num1, num2, total))
+        return total
+
+    options = {
+            options = {
+                "+": my_add,
+                "-": my_sub,
+                "*": my_mul,
+                "/": my_div
+
+
