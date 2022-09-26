3c5053ee1de07398066572855212873e63df5ec1
Geoffy1 <geojogu@gmail.com>
2022-09-26 11:18:13 -0700

    finds the biggest integer of a list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
+    maxi = 0
     if my_list is None or len(my_list) is 0:
         return "None"
-    max = my_list[0]
     for numberz in range(len(my_list)):
-        if my_list[numberz] > max:
-            max = my_list[numberz]
-    return max
+        if my_list[numberz] > maxi:
+            maxi = my_list[numberz]
+    return maxi
