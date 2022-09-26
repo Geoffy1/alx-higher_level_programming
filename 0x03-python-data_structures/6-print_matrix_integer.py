8a8cf4834d7a15c9225ff10b0a1ba2726f6b088f
Geoffy1 <geojogu@gmail.com>
2022-09-26 13:34:00 -0700

    prints a matrix of integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for sublist in matrix:
-        for colu in sublist:
-             print("{:d}".format(colu), end=" " if colu != sublist[-1] else "")
+        for element in range(len(sublist)):
+            print("{:d}".format(sublist[element]), end=" ")
+            if element != len(sublist) - 1:
+                print(" ", end="")
         print("")
