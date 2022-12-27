f5ff4341b08fb2d2d4802462833e55471194f388
Geoffy1 <106971868+Geoffy1@users.noreply.github.com>
2022-12-28 02:12:35 +0300

    Create 5-filter_cities.py
     a script that takes in the name of a state as an argument and lists all cities of that state, using the database hbtn_0e_4_usa

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+This script  takes in the name of a state
+as an arg and lists all cities of that
+state, using the db `hbtn_0e_4_usa`.
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    Access to the db and get the cities
+    from the db.
+    """
+
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+
+    with db.cursor() as cus:
+        cus.execute("""
+            SELECT
+                cities.id, cities.name
+            FROM
+                cities
+            JOIN
+                states
+            ON
+                cities.state_id = states.id
+            WHERE
+                states.name LIKE BINARY %(state_name)s
+            ORDER BY
+                cities.id ASC
+        """, {
+            'state_name': argv[4]
+        })
+
+        rows = cus.fetchall()
+
+    if rows is not None:
+        print(", ".join([row[1] for row in rows]))
