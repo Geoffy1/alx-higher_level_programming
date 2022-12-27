1f5d87dca11e82bc00f67f457e9803dfa9435355
Geoffy1 <106971868+Geoffy1@users.noreply.github.com>
2022-12-28 02:34:12 +0300

    Create relationship_city.py
    Improve the files model_city.py

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This script defs a City cls
+to work with MySQLAlchemy ORM.
+"""
+
+from relationship_state import Base, State
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+
+class City(Base):
+    """City class
+    Attr:
+        __tablename__ (str): The table name of the cls
+        id (int): The id of the cls
+        name (str): The name of the cls
+        state_id (int): The state the city belongs
+    """
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
