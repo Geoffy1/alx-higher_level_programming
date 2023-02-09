4a9b0521b48d61a3ce0ad7b851d16c6301b0aaff
Geoffy1 <106971868+Geoffy1@users.noreply.github.com>
2023-02-09 23:12:59 +0300

    Update 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@
 const filename = process.argv[2];
 const text = process.argv[3];
 const fs = require('fs');
-fs.writeFile(filename, text, 'utf8', (err)=> {
+fs.writeFile(filename, text, 'utf8', (err) => {
   if (err) {
     console.log(err);
   }
