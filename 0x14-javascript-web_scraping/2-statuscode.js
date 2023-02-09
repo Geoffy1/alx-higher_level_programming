fe78f39d79f08899c4a1ff9bfa6ff2dff22f92df
Geoffy1 <106971868+Geoffy1@users.noreply.github.com>
2023-02-09 23:14:54 +0300

    Update 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,5 +2,4 @@
 
 const request = require('request');
 
-request(process.argv[2], (_err, res)=> return console.log('code:', res.statusCode); // Print the response status code if a response was received
-  );
+request(process.argv[2], (_err, res) => return console.log('code:', res.statusCode); // Print the response status code if a response was received);
