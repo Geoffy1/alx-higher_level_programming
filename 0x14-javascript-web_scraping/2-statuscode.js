740605c4ac80f9ae10f03f0d1cb5de6ab98ba130
Geoffy1 <106971868+Geoffy1@users.noreply.github.com>
2023-02-09 23:00:03 +0300

    Create 2-statuscode.js

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request(process.argv[2], (_err, res)=> return console.log('code:', res.statusCode); // Print the response status code if a response was received
+  );
