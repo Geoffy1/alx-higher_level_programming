d059135f7598852c8f5406335bc928efbe007100
Geoffy1 <106971868+Geoffy1@users.noreply.github.com>
2023-02-09 23:17:07 +0300

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 
-request(process.argv[2], (err, _res, body)=> {
+request(process.argv[2], (err, _res, body) => {
   if (err) {
     console.log(err);
   } else {
